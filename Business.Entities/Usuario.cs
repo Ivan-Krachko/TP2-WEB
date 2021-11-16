37593b8f75388f26f89de622eabb436497412d3b
Krachko Ivan <ivank.-@hotmail.com>
2021-11-16 15:00:04 -0300

    Version Final

--- Business.Entities/Usuario.cs
+++ Business.Entities/Usuario.cs
@@ -82,5 +82,7 @@ namespace Business.Entities
                 _Habilitado = value;
             }
         }
+
+
     }
 }
--- Data.Database/UsuarioAdapter.cs
+++ Data.Database/UsuarioAdapter.cs
@@ -351,4 +351,5 @@ namespace Data.Database
             }
         }
     }
+
 }
